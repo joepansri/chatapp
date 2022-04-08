35674f679a8e4bb2a00d5935536d850cea06fa9c
Joe Pansri <joe.pansri@gmail.com>
2022-04-08 21:49:19 +0900

    add: message to talkroom

--- lib/pages/talk_room.dart
+++ lib/pages/talk_room.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
+import 'package:udemychat/model/message.dart';
 
 class TalkRoom extends StatefulWidget {
   final String name;
@@ -9,12 +11,36 @@ class TalkRoom extends StatefulWidget {
 }
 
 class _TalkRoomState extends State<TalkRoom> {
+  List<Message> messageList = [
+    Message(
+        message: 'あいうえお', isMe: true, sendTime: DateTime(2022, 4, 8, 01, 58)),
+    Message(
+        message: 'かきくけこ', isMe: false, sendTime: DateTime(2022, 4, 8, 01, 59)),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
       appBar: AppBar(
         title: Text(widget.name),
       ),
+      body: ListView.builder(
+          itemCount: messageList.length,
+          itemBuilder: (context, index) {
+            return Row(
+              textDirection: messageList[index].isMe!
+                  ? TextDirection.rtl
+                  : TextDirection.ltr,
+              children: [
+                Container(
+                    color:
+                        messageList[index].isMe! ? Colors.green : Colors.white,
+                    child: Text(messageList[index].message!)),
+                Text(intl.DateFormat('HH:mm:')
+                    .format(messageList[index].sendTime!))
+              ],
+            );
+          }),
     );
   }
 }
